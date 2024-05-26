9bde5d05b8a85f7a9b5317a459c5679c8066ca59
Alessandro-DiGiusto <digiusto.alessandro@icloud.com>
2024-05-27 00:08:38 +0200

    update UI button, add conditions, fix hide keyboard on background tap, Add bottom banner with 5 icon: settings, profile, Add, Show Patients list, info

--- MedicaInfo/ContentView.swift
+++ MedicaInfo/ContentView.swift
@@ -1,42 +1,12 @@
-//
-//  ContentView.swift
-//  MedicaInfo
-//
-//  Created by Alessandro Di Giusto on 22/05/24.
-//
-
 import SwiftUI
 import CoreData
 import UserNotifications
-
-// Definizione di RadioButtonField
-struct RadioButtonField: View {
-    let id: ContentView.Gender
-    let label: String
-    let callback: (ContentView.Gender) -> ()
-    let isMarked: Bool
-    
-    var body: some View {
-        Button(action: {
-            self.callback(self.id)
-        }) {
-            HStack(alignment: .center) {
-                Image(systemName: isMarked ? "largecircle.fill.circle" : "circle")
-                    .foregroundColor(isMarked ? Color.blue : Color.primary)
-                Text(label)
-                    .foregroundColor(Color.primary)
-                Spacer()
-            }
-        }
-        .foregroundColor(Color.white)
-    }
-}
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
     @FetchRequest(entity: Patient.entity(), sortDescriptors: [])
     private var patients: FetchedResults<Patient>
-
+    
     @State private var name: String = ""
     @State private var surname: String = ""
     @State private var birthDate: Date = Date()
@@ -47,111 +17,179 @@ struct ContentView: View {
     @State private var showDetailView = false
     @State private var showConfirmationAlert = false
     
-    @State private var isKeyboardHidden = true // Aggiunto stato per gestire la visibilità della tastiera
+    @State private var amount: String = ""
+    @FocusState private var amountIsFocused: Bool
     
     enum Gender: String, CaseIterable {
         case male = "Maschile"
         case female = "Femminile"
     }
-
+    
     let availableConditions = [
-        "Ipertensione",
-        "Diabete",
-        "Asma",
-        "Artrite",
-        "Micrania",
-        "Obesità",
-        "Sovrappeso",
-        "Diabete di tipo 1",
-        "Diabete di tipo 2",
-        "Diabete M.O.D.Y",
-        "Ipertensione",
-        "Dislipidemia",
-        "Sindrome metabolica",
-        "Intolleranze alimentari",
-        "Celiachia",
-        "Allergie alimentari",
-        "Dieta chetogenica",
-        "Dieta mediterranea",
-        "Dieta a basso contenuto di carboidrati",
-        "Dieta a basso contenuto di grassi",
-        "Ipercolesterolemia",
-        "Ipertrigliceridemia",
-        "Anemia da carenza di ferro",
-        "Anemia megaloblastica",
-        "Iperuricemia",
-        "Malnutrizione",
-        "Intolleranza al lattosio"
+        "Ipertensione", "Diabete", "Asma", "Artrite", "Micrania", "Obesità",
+        "Sovrappeso", "Diabete di tipo 1", "Diabete di tipo 2", "Diabete M.O.D.Y",
+        "Ipertensione", "Dislipidemia", "Sindrome metabolica", "Intolleranze alimentari",
+        "Celiachia", "Allergie alimentari", "Dieta chetogenica", "Dieta mediterranea",
+        "Dieta a basso contenuto di carboidrati", "Dieta a basso contenuto di grassi",
+        "Ipercolesterolemia", "Ipertrigliceridemia", "Anemia da carenza di ferro",
+        "Anemia megaloblastica", "Iperuricemia", "Malnutrizione", "Intolleranza al lattosio"
     ]
-
+    
     var body: some View {
         NavigationView {
-            Form {
-                Section(header: Text("Informazioni Personali")) {
-                    TextField("Nome", text: $name)
-                    TextField("Cognome", text: $surname)
-                    DatePicker("Data di Nascita", selection: $birthDate, displayedComponents: .date)
-                    TextField("Codice Fiscale", text: $cf)
-                    
-                    HStack {
-                        Text("Sesso")
-                        Spacer()
-                        Picker("Gender", selection: $selectedGender) {
-                            ForEach(Gender.allCases, id: \.self) { gender in
-                                Text(gender.rawValue)
-                            }
-                        }
-                        .pickerStyle(SegmentedPickerStyle())
+            ZStack {
+                Color(UIColor.systemGroupedBackground)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        amountIsFocused = false
                     }
-                }
                 
-                Section(header: Text("Condizioni Mediche")) {
-                    VStack {
-                        Picker("Condizioni Rilevanti", selection: $selectedConditionIndex) {
-                            ForEach(0..<availableConditions.count, id: \.self) {
-                                Text(availableConditions[$0])
-                            }
-                        }
-                        .pickerStyle(DefaultPickerStyle())
-                        .onChange(of: selectedConditionIndex) { newValue in
-                            addCondition(newValue)
+                VStack {
+                    Form {
+                        Section(header: Text("Informazioni Personali")) {
+                            TextField("Nome", text: $name)
+                                .focused($amountIsFocused)
+                            TextField("Cognome", text: $surname)
+                                .focused($amountIsFocused)
+                            DatePicker("Data di Nascita", selection: $birthDate, displayedComponents: .date)
+                            TextField("Codice Fiscale", text: $cf)
+                                .focused($amountIsFocused)
                         }
                         
-                        if !conditions.isEmpty {
-                            Text("Patologie selezionate:")
-                            ForEach(conditions, id: \.self) { condition in
-                                Text(condition)
+                        Section(header: Text("Condizioni Mediche")) {
+                            VStack {
+                                Picker("Condizioni Rilevanti", selection: $selectedConditionIndex) {
+                                    ForEach(0..<availableConditions.count, id: \.self) {
+                                        Text(availableConditions[$0])
+                                    }
+                                }
+                                .pickerStyle(DefaultPickerStyle())
+                                .onChange(of: selectedConditionIndex) { newValue in
+                                    addCondition(newValue)
+                                }
+                                
+                                if !conditions.isEmpty {
+                                    Text("Patologie selezionate:")
+                                    ForEach(conditions, id: \.self) { condition in
+                                        Text(condition)
+                                    }
+                                }
+                            }
+                        }
+                        
+                        Section(header: Text("")) {
+                                Button(action: saveData) {
+                                    Text("Salva")
+                                        .frame(maxWidth: .infinity, alignment: .center)
+                                        .padding()
+                                        .background(Color.blue)
+                                        .foregroundColor(.white)
+                                        .cornerRadius(10)
+                                }
+                                
+                                Button(action: {
+                                    showDetailView.toggle()
+                                }) {
+                                    Text("Visualizza Dati Salvati")
+                                        .frame(maxWidth: .infinity, alignment: .center)
+                                        .padding()
+                                        .background(Color.green)
+                                        .foregroundColor(.white)
+                                        .cornerRadius(10)
+                                }
+                                .sheet(isPresented: $showDetailView) {
+                                    DetailView(onDeleteAllPatients: deleteAllPatients)
+                                        .environment(\.managedObjectContext, viewContext)
+                                }
+                            }
+                            
+                            Section(header: Text("Dev by alessandrodigiusto.it")) {
+                            }
+                    }
+                    .navigationTitle("MedicaInfo")
+                    .toolbar {
+                        if amountIsFocused {
+                            ToolbarItem(placement: .keyboard) {
+                                Button("Done") {
+                                    amountIsFocused = false
+                                }
+                            }
+                        }
+                    }
+                    .toolbar {
+                        ToolbarItem(placement: .bottomBar) {
+                            HStack {
+                                Spacer()
+                                
+                                // Primo tasto: Impostazioni
+                                Button(action: {
+                                    // Azione per il primo tasto
+                                }) {
+                                    Image(systemName: "gearshape.fill")
+                                    Text("")
+                                }
+                                .padding()
+                                
+                                Spacer()
+                                
+                                // Secondo tasto: Profilo
+                                Button(action: {
+                                    // Azione per il secondo tasto
+                                }) {
+                                    Image(systemName: "person.fill")
+                                    Text("")
+                                }
+                                .padding()
+                                
+                                Spacer()
+                                
+                                // Terzo tasto: Aggiungi
+                                Button(action: {
+                                    // Azione per il terzo tasto
+                                }) {
+                                    Image(systemName: "plus.circle.fill")
+                                    Text("")
+                                }
+                                .padding()
+                                
+                                Spacer()
+                                
+                                // Quarto tasto: Elenco
+                                Button(action: {
+                                    // Azione per il quarto tasto
+                                }) {
+                                    Image(systemName: "list.bullet")
+                                    Text("")
+                                }
+                                .padding()
+                                
+                                Spacer()
+                                
+                                // Quinto tasto: Info
+                                Button(action: {
+                                    // Azione per il quinto tasto
+                                }) {
+                                    Image(systemName: "info.circle.fill")
+                                    Text("")
+                                }
+                                .padding()
+                                
+                                Spacer()
                             }
                         }
                     }
                 }
-                
-                Button(action: saveData) {
-                    Text("Salva")
-                        .frame(maxWidth: .infinity, alignment: .center)
-                }
-                
-                Button(action: {
-                    showDetailView.toggle()
-                }) {
-                    Text("Visualizza Dati Salvati")
-                        .frame(maxWidth: .infinity, alignment: .center)
-                }
-                .sheet(isPresented: $showDetailView) {
-                    DetailView(onDeleteAllPatients: deleteAllPatients)
-                        .environment(\.managedObjectContext, viewContext)
-                }
+                .padding()
             }
-            .navigationTitle("MedicaInfo")
-        }
-        .alert(isPresented: $showConfirmationAlert) {
-            Alert(
-                title: Text("Salvataggio completato"),
-                message: Text("I dati sono stati salvati con successo."),
-                dismissButton: .default(Text("OK")) {
-                    clearForm()
-                }
-            )
+            .alert(isPresented: $showConfirmationAlert) {
+                Alert(
+                    title: Text("Salvataggio completato"),
+                    message: Text("I dati sono stati salvati con successo."),
+                    dismissButton: .default(Text("OK")) {
+                        clearForm()
+                    }
+                )
+            }
         }
     }
     
@@ -171,8 +209,7 @@ struct ContentView: View {
 
         do {
             try viewContext.save()
-            //clearForm()
-            scheduleNotificationAndShowAlert() // Aggiunta della chiamata alla funzione scheduleNotification
+            scheduleNotificationAndShowAlert()
         } catch {
             print("Failed to save data: \(error)")
         }
@@ -215,3 +252,4 @@ struct ContentView: View {
         }
     }
 }
+
